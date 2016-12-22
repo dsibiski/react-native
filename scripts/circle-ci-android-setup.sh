3a252e9a00a45f52f64b382943c36b30b8c4ea4b
Dave Sibiski <dsibiski@users.noreply.github.com>
2016-12-22 10:08:37 -0400

    Update circle-ci-android-setup.sh

--- scripts/circle-ci-android-setup.sh
+++ scripts/circle-ci-android-setup.sh
@@ -12,6 +12,7 @@ function getAndroidSDK {
     echo y | android update sdk -u -a -t extra-android-m2repository &&
     echo y | android update sdk -u -a -t extra-android-support &&
     echo y | android update sdk -u -a -t extra-google-m2repository &&
+    echo y | android update sdk -u -a -t sys-img-armeabi-v7a-android-19,sys-img-x86_64-android-19 &&
     echo no | android create avd -n testAVD -f -t android-19 --abi default/armeabi-v7a &&
     touch $DEPS
   fi
